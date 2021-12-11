00973adaf46ae018eba30ead8158eeb6a9ad7eaa
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 11:40:11 -0500

    Update netsize.sh
    Arreglo de ortografía

--- Tarea N°1/netsize.sh
+++ Tarea N°1/netsize.sh
@@ -1,9 +1,9 @@
 # Primero abrimos la ruta donde está ubicada la carpeta Saavedra2013 en su computador y luego:
 cd Saavedra2013
-# Posteriormente ejecutar 
+# Seguido ejecutamos el comando;
 cat n1.txt
 # Finalmente se obtiene el numero de filas y columnas en el siguiente código:
-echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El numero de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
-# Se obtiene el resuldato
+echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El número de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
+# Se obtiene el resuldato;
 # El numero de columnas es:80
 # El numero de filas es: 97 
