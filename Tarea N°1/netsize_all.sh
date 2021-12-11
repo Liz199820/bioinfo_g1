3ca0a549a96f7185a1d942502a51a848b490c610
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 18:45:23 -0500

    Update netsize_all.sh
    Arreglo del comando

--- Tarea N°1/netsize_all.sh
+++ Tarea N°1/netsize_all.sh
@@ -1,14 +1,6 @@
 # Tarea del ejercicio 1.10.3 parte 2
-#Primero, crea la variable archivo con todos los *.txt de la carpeta Saavedra2013
+#Primero, abrir el directorio del archivo Saavedra2013
 archivo=*.txt
-
-#echo: este comando nos ayuda a imprimir lo que contiene nuestra variable
-echo $archivo
-
 # crear loop, donde:
-for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c; done
-# Comando para guardar mi archivo
-for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c >> ../../../../netsize_all.txt; done
-
-# Comando que muestra los archivos y listo
-cat ../../../../netsize_all.txt
+for file in $(1s *.txt); do wc -l $file; head -n 1 $file | grep -o " " | wc -1; done
+# Se obtienen el numero de filas y columnas de todos los archivos .txt
