d2d6b26323a99ccdb57a178cf9d525ca9681eeb4
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 01:40:43 -0500

    Update netsize.sh
    Apliqué  el Comando grep -c cuenta las veces que se tiene una palabra de nuestro interés, y el wc -1 el cual nos ayuda a contar solo las líneas

--- Tarea N°1/netsize.sh
+++ Tarea N°1/netsize.sh
@@ -1,6 +1,9 @@
-#Primero abrimos la ruta donde está ubicada la carpeta Saavedra2013 en su computador y luego:
+# Primero abrimos la ruta donde está ubicada la carpeta Saavedra2013 en su computador y luego:
 cd Saavedra2013
-#Posteriormente ejecutar 
+# Posteriormente ejecutar 
 cat n1.txt
-#Finalmente se obtiene el numero de filas y columnas en el siguiente código:
+# Finalmente se obtiene el numero de filas y columnas en el siguiente código:
 echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El numero de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
+# Se obtiene el resuldato
+# El numero de columnas es:80
+# El numero de filas es: 97 
