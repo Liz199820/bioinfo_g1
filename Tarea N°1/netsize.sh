903c5ebe099223350bafb89fde4509cb50bc7600
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 13:43:30 -0500

    Update netsize.sh
    Delimitación de los comandos

--- Tarea N°1/netsize.sh
+++ Tarea N°1/netsize.sh
@@ -1,9 +1,12 @@
 # Primero abrimos la ruta donde está ubicada la carpeta Saavedra2013 en su computador y luego:
-cd Saavedra2013
+# cd:ubicación de la data en mi directorio
+cd Documents/6to/Bioinformática/CSB-master/unix/data/Saavedra2013
 # Seguido ejecutamos el comando;
 cat n1.txt
 # Finalmente se obtiene el numero de filas y columnas en el siguiente código:
 echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El número de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
-# Se obtiene el resuldato;
+# #echo: imprime lo indicado dentro de " ", >: guarda lo que se imprime en el
+#archivo,>>: guarda lo que se imprime sin borrar lo anterior en el mismo archivo.
+Se obtiene el resuldato;
 # El numero de columnas es:80
 # El numero de filas es: 97 
