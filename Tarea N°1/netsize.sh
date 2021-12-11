072c7cdf23c035f0db8f0c42e5c7b69e5d5c9d81
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 01:30:14 -0500

    Update netsize.sh
    Aplicación del comando echo: este nos ayuda a imprimir leyendas

--- Tarea N°1/netsize.sh
+++ Tarea N°1/netsize.sh
@@ -2,3 +2,5 @@
 cd Saavedra2013
 #Posteriormente ejecutar 
 cat n1.txt
+#Finalmente se obtiene el numero de filas y columnas en el siguiente código:
+echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El numero de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
