3b21825d365245b23c6b33b0bb701b2690f8761d
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 14:48:11 -0500

    Update netsize_all.sh
    Creación de archivo

--- Tarea N°1/netsize_all.sh
+++ Tarea N°1/netsize_all.sh
@@ -1,3 +1,5 @@
 # Tarea del ejercicio 1.10.3 parte 2
 #Primero, crea la variable archivo con todos los *.txt de la carpeta Saavedra2013
 archivo=*.txt
+#echo: este comando nos ayuda a imprimir lo que contiene nuestra variable
+echo $archivo
