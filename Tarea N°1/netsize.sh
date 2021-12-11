504d333b4fd5fbc49e3b34ce2823d9c23ad089e5
Lizeth Garcia <94251330+Liz199820@users.noreply.github.com>
2021-12-11 18:46:39 -0500

    Update netsize.sh
    Cambio de resultado en .txt

--- Tarea N°1/netsize.sh
+++ Tarea N°1/netsize.sh
@@ -4,9 +4,8 @@ cd Documents/6to/Bioinformática/CSB-master/unix/data/Saavedra2013
 # Seguido ejecutamos el comando;
 cat n1.txt
 # Finalmente se obtiene el numero de filas y columnas en el siguiente código:
-echo "El numero de columnas es;" ; head -n1 n1.txt | grep -c " " | wc -1 ; echo "El número de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
+echo "El numero de columnas es;" ; head -n1 n1.txt | grep -o " " | wc -1 ; echo "El número de filas es" ; wc -1 n1.txt; eco "Fin de la ejecución"
 # #echo: imprime lo indicado dentro de " ", >: guarda lo que se imprime en el
 #archivo,>>: guarda lo que se imprime sin borrar lo anterior en el mismo archivo.
 Se obtiene el resuldato;
-# El numero de columnas es:80
-# El numero de filas es: 97 
+ 
